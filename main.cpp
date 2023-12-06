8c3cac3da2655e625e9b5d401d946207819842ec
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-12-06 09:19:12 +0100

    début

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <pcap.h>
+#include <iostream>
+
+void packetHandler(unsigned char* userData, const struct pcap_pkthdr* pkthdr, const unsigned char* packetData) {
+    // Traitez ici les données de la trame
+    std::cout << "Capture d'une trame de taille : " << pkthdr->len << " octets" << std::endl;
+}
+
+int main() {
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    // Ouvrir une interface de capture
+    pcap_t* handle = pcap_open_live("your_network_interface", 65536, 1, 1000, errbuf);
+
+    if (handle == nullptr) {
+        std::cerr << "Erreur lors de l'ouverture de l'interface: " << errbuf << std::endl;
+        return -1;
+    }
+
+    // Capturer les paquets en utilisant le callback packetHandler
+    pcap_loop(handle, 0, packetHandler, nullptr);
+
+    // Fermer l'interface de capture
+    pcap_close(handle);
+
+    return 0;
+}
